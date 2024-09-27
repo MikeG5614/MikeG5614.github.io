2b5a8f541e91980e0253dafc416de2ebaab8052a
Mike <mike@Mikes-MacBook-Pro.local>
2024-09-27 11:32:11 -0400

    commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('modeToggle').addEventListener('change', function() {
     document.body.classList.toggle('dark-theme', this.checked);
     const label = document.querySelector('.label');
-    label.classList.toggle('dark', this.checked); // Add/remove dark class for background
+    label.classList.toggle('dark', this.checked);
 });
